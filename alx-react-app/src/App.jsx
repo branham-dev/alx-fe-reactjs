9bb862b4faf57c2f995effd4f6b4f4275e068362
Branham Karanja <branham.karanja@outlook.com>
2025-02-12 23:29:28 +0300

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
 	return (
 		<>
 			<WelcomeMessage />
+			<Header />
+			<MainContent />
+			<Footer />
 		</>
 	);
 }
