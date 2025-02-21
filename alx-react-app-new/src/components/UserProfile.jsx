2880edd61a449bb8ea49e86a6a2706e6a470f96c
Branham Karanja <branham.karanja@outlook.com>
2025-02-21 17:03:31 +0300

    corrections

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 const UserProfile = (props) => {
 	return (
-		<div style={{ backgroundColor: "#e5e5e5", border: "1px solid #000", padding: "10px", margin: "10px" }}>
-			<h2 style={{ color: "#14213d" }}>{props.name}</h2>
-			<p style={{ color: "" }}>
+		<div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+			<h2 style={{ color: "blue" }}>{props.name}</h2>
+			<p>
 				Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
 			</p>
-			<p style={{ color: "" }}>Bio: {props.bio}</p>
+			<p>Bio: {props.bio}</p>
 		</div>
 	);
 };
