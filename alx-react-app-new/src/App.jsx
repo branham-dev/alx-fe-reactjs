52ecc392c6f78c9a4c82cf72888be0ac30c4e029
Branham Karanja <branham.karanja@outlook.com>
2025-02-21 17:07:33 +0300

    corrections

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
@@ -12,6 +13,7 @@ function App() {
 			<WelcomeMessage />
 			<Header />
 			<MainContent />
+			<Counter />
 			<Footer />
 		</>
 	);
