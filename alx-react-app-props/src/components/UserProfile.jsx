2de65a71cb6e6066ffd6a3fa34c93585dd928bb7
Branham Karanja <branham.karanja@outlook.com>
2025-02-21 20:06:37 +0300

    corrections

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-const UserProfile = (props) => {
+import { useContext } from "react";
+import userContext from "./UserContext";
+
+const UserProfile = () => {
+	const userData = useContext(userContext);
 	return (
 		<div>
-			<h2>{props.name}</h2>
-			<p>Age: {props.age}</p>
-			<p>Bio: {props.bio}</p>
+			<h2>{userData.name}</h2>
+			{/* <p>Age: {age}</p>
+			<p>Bio: {bio}</p> */}
 		</div>
 	);
 };
