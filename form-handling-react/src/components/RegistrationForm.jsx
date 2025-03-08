54ccd8100661afd1b1eb02774455f2909c2a8879
Branham Karanja <branham.karanja@outlook.com>
2025-03-08 23:22:25 +0300

    corrections

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+
 const RegistrationForm = () => {
-  return (
-    <div>RegistrationForm</div>
-  )
-}
-export default RegistrationForm
+	const [formData, setFormData] = useState({});
+
+	const handleChange = (e) => {
+		const { name, value } = e;
+
+		setFormData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (formData.name && formData.email && formData.password) {
+            alert("submitted")
+        }
+    }
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type='text' name='name' value={formData.name} onChange={handleChange} />
+			<input type='email' name='email' value={formData.email} onChange={handleChange} />
+			<input type='password' name='password' value={formData.password} onChange={handleChange} />
+		</form>
+	);
+};
+export default RegistrationForm;
