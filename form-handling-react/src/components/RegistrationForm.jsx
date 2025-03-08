ef6c804ac28cb19a3494b7c00f8a2db6d57de657
Branham Karanja <branham.karanja@outlook.com>
2025-03-08 23:24:33 +0300

    corrections

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const RegistrationForm = () => {
-	const [formData, setFormData] = useState({});
+	const [formData, setFormData] = useState({ username: "", email: "", password: "" });
 
 	const handleChange = (e) => {
 		const { name, value } = e;
@@ -10,19 +10,19 @@ const RegistrationForm = () => {
 			...prev,
 			[name]: value,
 		}));
-    };
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
+	};
 
-        if (formData.name && formData.email && formData.password) {
-            alert("submitted")
-        }
-    }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (formData.username && formData.email && formData.password) {
+			alert("submitted");
+		}
+	};
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<input type='text' name='name' value={formData.name} onChange={handleChange} />
+			<input type='text' name='username' value={formData.username} onChange={handleChange} />
 			<input type='email' name='email' value={formData.email} onChange={handleChange} />
 			<input type='password' name='password' value={formData.password} onChange={handleChange} />
 		</form>
