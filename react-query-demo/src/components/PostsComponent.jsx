af01810f0a72a128947a132612ba20dfede79593
Branham Karanja <branham.karanja@outlook.com>
2025-03-09 18:31:20 +0300

    implement react-query

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,16 +1,35 @@
 import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const PostsComponent = () => {
-	const fetchData = async () => {
-		const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-		const data = await response.json();
-		console.log(data);
-	};
+	// const fetchData = async () => {
+	// 	const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+	// 	const data = await response.json();
+	// 	console.log(data);
+	// };
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+	// useEffect(() => {
+	// 	fetchData();
+	// }, []);
 
-	return <div>PostsComponent</div>;
+	const { isPending, error, data } = useQuery({
+		queryKey: ["posts"],
+		queryFn: () => fetch("https://jsonplaceholder.typicode.com/posts").then((res) => res.json()),
+	});
+
+	if (isPending) return <div>Loading...</div>;
+
+	if (error) return <div>An error has occurred: {error.message}</div>;
+
+	return (
+		<div>
+			{data.map((post) => (
+				<div key={post.id}>
+					<h2>{post.title}</h2>
+					<p>{post.body}</p>
+				</div>
+			))}
+		</div>
+	);
 };
 export default PostsComponent;
