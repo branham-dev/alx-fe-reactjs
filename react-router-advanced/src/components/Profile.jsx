3fa5b0d6708746bf985dc83cb9872cf60c190c3c
Branham Karanja <branham.karanja@outlook.com>
2025-03-09 20:07:37 +0300

    advanced routing

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
 	return (
 		<div>
+			<div>
+				<ProfileDetails />
+				<ProfileSettings />
+			</div>
 			<Outlet />
 		</div>
 	);
