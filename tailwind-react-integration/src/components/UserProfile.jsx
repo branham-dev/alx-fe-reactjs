ebbef37f460162327960313fa7c090a92a0e442f
Branham Karanja <branham.karanja@outlook.com>
2025-03-11 22:16:08 +0300

    corrections

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
 	return (
-		<div className='user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm'>
+		<div className='user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm'>
 			<img
 				src='https://via.placeholder.com/150'
 				alt='User'
