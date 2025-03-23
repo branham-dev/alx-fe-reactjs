feac8865d5072082ee970ba3c96544e64a45b92c
Branham Karanja <branham.karanja@outlook.com>
2025-03-23 21:03:45 +0300

    corrections

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,11 +1,16 @@
 import { useParams } from "react-router-dom";
 import data from "../data.json";
+import { useEffect, useState } from "react";
 
 const RecipeDetail = () => {
 	const { id } = useParams();
+	const [recipe, setRecipe] = useState({});
 
-	const recipe = data.find((recipe) => recipe.id === Number(id));
-	// console.log(recipe);
+	useEffect(() => {
+		const recipeData = data.find((recipe) => recipe.id === Number(id));
+		setRecipe(recipeData);
+		// console.log(recipe);
+	}, []);
 
 	return (
 		<>
@@ -13,6 +18,11 @@ const RecipeDetail = () => {
 				<article className='bg-[#262424] w-[75%] m-auto py-8 px-6 text-[#eee5da]'>
 					<h1 className='text-2xl mb-4'>{recipe.title}</h1>
 					<p>{recipe.summary}</p>
+					<img src={recipe.image} alt='recipe' />
+
+					<h2>Ingredients</h2>
+					<h2>instructions</h2>
+					{/* <h2>Ingredients</h2> */}
 				</article>
 			</div>
 		</>
