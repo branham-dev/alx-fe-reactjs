9e427100182f15fef2c210082d21c80cca5fa623
Branham Karanja <branham.karanja@outlook.com>
2025-03-24 22:39:55 +0300

    enhance user interface and add advanced search features - corrections

--- github-user-search/src/components/Layout.jsx
+++ github-user-search/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchUsers } from "../services/githubService";
-import SearchForm from "./SearchForm";
+import SearchForm from "./Search";
 
 const Layout = () => {
 	const navigate = useNavigate();
